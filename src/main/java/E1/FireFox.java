d4fdd35ec0aa4bd6a6b174ac1c9f12adedf06b88
TSherif01 <Tesneemsherif@gmail.com>
2024-09-20 17:47:39 -0400

    Initial commit

---
+++ src/main/java/E1/FireFox.java
@@ -0,0 +1,27 @@
+package E1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFox {
+    public static void main(String[] args) {
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.syntaxprojects.com/");
+
+        driver.manage().window().maximize();
+
+        String url = driver.getCurrentUrl();
+
+        String title = driver.getTitle();
+
+        System.out.println("The title of the webpage:" + title);
+        System.out.println("The url of the webpage:" + url);
+
+        driver.quit();
+        }
+    }
+
+
+
