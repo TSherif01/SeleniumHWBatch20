61620bfb9af86a481d7586edc2c4a5e91d10b1ea
TSherif01 <Tesneemsherif@gmail.com>
2024-09-20 17:59:11 -0400

    done.

--- src/main/java/E1/WebDriverCommands.java
+++ src/main/java/E1/WebDriverCommands.java
@@ -1,5 +1,24 @@
 package E1;
 
-public class WebDriverCommands {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class WebDriverCommands {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://google.com");
+
+        driver.navigate().to("https://www.syntaxprojects.com/");
+
+        driver.navigate().back();
+
+        driver.navigate().refresh();
+
+        driver.quit();
+
+    }
 }
