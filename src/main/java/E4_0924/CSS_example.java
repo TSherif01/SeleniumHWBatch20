575c4f331a40d4d4da68a4553cadf2cd1cda626b
TSherif01 <Tesneemsherif@gmail.com>
2024-09-24 21:35:56 -0400

    done.

--- src/main/java/E4_0924/CSS_example.java
+++ src/main/java/E4_0924/CSS_example.java
@@ -1,6 +1,8 @@
 package E4_0924;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CSS_example {
@@ -8,6 +10,25 @@ public class CSS_example {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://syntaxprojects.com/Xpath.php");
+        driver.get("http://syntaxprojects.com/cssSelector.php");
+
+        WebElement profileID = driver.findElement(By.cssSelector("input[id='profileID']"));
+        profileID.sendKeys("12345");
+
+        WebElement profileBx = driver.findElement(By.cssSelector("input#profileBox"));
+        profileBx.sendKeys("tsherif");
+
+        WebElement Jenny = driver.findElement(By.cssSelector("input.form-control.feedbackBox1"));
+        Jenny.sendKeys("good");
+
+        WebElement course = driver.findElement(By.cssSelector("input[name*='contentInput']"));
+        course.sendKeys("syntax selenium");
+
+        WebElement advancedCSS = driver.findElement(By.cssSelector("input[name^='IntroInput']"));
+        advancedCSS.sendKeys("name^ -> startsWith");
+
+        WebElement conclusion = driver.findElement(By.cssSelector("input[name$='Input']']"));
+        conclusion.sendKeys("name$ -> endsWith");
+
     }
 }
--- src/main/java/E4_0924/CSS_hw.java
+++ src/main/java/E4_0924/CSS_hw.java
@@ -1,8 +1,37 @@
 package E4_0924;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CSS_hw {
     public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://syntaxprojects.com/cssSelector-homework.php");
+
         //1. The user should fill in all the text boxes in the form using
         // *CSS selectors that are unique and return only one element (1/1).
+
+        WebElement userID = driver.findElement(By.cssSelector("input[id='UserID']"));
+        userID.sendKeys("12345");
+
+        WebElement userName = driver.findElement(By.cssSelector("input#UserName"));
+        userName.sendKeys("tsherif");
+
+        WebElement Ray = driver.findElement(By.cssSelector("input.form-control.feedbackBox1"));
+        Ray.sendKeys("good");
+
+        WebElement clientID = driver.findElement(By.cssSelector("input[name*='ClientID']"));
+        clientID.sendKeys("syntax selenium");
+
+        WebElement email = driver.findElement(By.cssSelector("input[name^='em']"));
+        email.sendKeys("name^ -> startsWith");
+
+        WebElement conclusion = driver.findElement(By.cssSelector("input[name$='Topic']"));
+        conclusion.sendKeys("name$ -> endsWith");
+
     }
 }
