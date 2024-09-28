a9679df0360b4425217c4a2a32484c439e5f16ab
TSherif01 <Tesneemsherif@gmail.com>
2024-09-27 20:28:16 -0400

    done.

--- src/main/java/E6_0927/CheckBoxes_example.java
+++ src/main/java/E6_0927/CheckBoxes_example.java
@@ -1,7 +1,11 @@
 package E6_0927;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class CheckBoxes_example {
     public static void main(String[] args) {
@@ -10,5 +14,20 @@ public class CheckBoxes_example {
         driver.get("http://syntaxprojects.com/basic-checkbox-demo.php");
         driver.manage().window().maximize();
 
+        WebElement cb1 = driver.findElement(By.xpath("//input[@id='isAgeSelected']"));
+        boolean stateofcb1 = cb1.isSelected();
+        if (!stateofcb1) {
+            cb1.click();
+        }
+
+        List <WebElement> cb2 = driver.findElements(By.xpath("//input[@class='cb1-element']"));
+        for (WebElement checkBox:cb2) {
+            if (checkBox.getAttribute("value").equals("Option-3")) {
+                checkBox.click();
+            }
+        }
+
+
     }
+
 }
--- src/main/java/E6_0927/CheckBoxes_hw.java
+++ src/main/java/E6_0927/CheckBoxes_hw.java
@@ -1,14 +1,56 @@
 package E6_0927;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class CheckBoxes_hw {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
-        driver.get("http://syntaxprojects.com/selenium_commands_selector-homework.php");
+        driver.get("http://syntaxprojects.com/basic-checkbox-demo-homework.php");
         driver.manage().window().maximize();
 
-    }
-}
+        /*
+        1. Subscribe to Newsletter:
+        - Check if the "Subscribe to Newsletter" checkbox is enabled, and then click on it. */
+        WebElement subscribe = driver.findElement(By.xpath("//input[@id='newsletter']"));
+        subscribe.click();
+
+        /*2.Select Your Hobbies:
+        - Retrieve the list of all checkboxes in the **Select Your Hobbies** section.
+        - Traverse through the list and select the **Reading** and **Cooking** checkboxes.*/
+        List<WebElement> hobbies = driver.findElements(By.xpath("//input[@class='cb-element']"));
+        for (WebElement checkBox:hobbies) {
+            if (checkBox.getAttribute("value").equals("reading")) {
+                checkBox.click();
+            }
+            if (checkBox.getAttribute("value").equals("cooking")) {
+                checkBox.click();
+            }
+        }
+
+        /*3. Select Your Interests:
+        - Verify that the **Support** and **Music** checkboxes are not displayed by default.
+        - Click on the "Show Checkboxes" button, and then select the **Music** checkbox.*/
+        WebElement show = driver.findElement(By.xpath("//button[@id='toggleCheckboxButtons']"));
+            show.click();
+
+        WebElement music = driver.findElement(By.xpath("//input[@type='checkbox'and@value='Music']"));
+        boolean stateOfmusic = music.isSelected();
+        if (!stateOfmusic) {
+            music.click();
+        }
+        /*4. Preferences:
+        - Verify that the **Receive Notifications** checkbox is disabled.
+        - Click on the "Enable Checkboxes" button, and then select the **Receive Notifications** checkbox.
+         */
+        WebElement enable2 = driver.findElement(By.xpath("//button[@id='enabledcheckbox']"));
+        enable2.click();
+        WebElement notifications = driver.findElement(By.xpath("//input[@value='Receive-Notifications']"));
+        notifications.click();
+            }
+        }
